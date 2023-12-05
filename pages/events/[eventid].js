3a29ff841a10016f250799588b06fb65f9bf85c9
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-12-05 13:26:18 +0100

    Event Detail page

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,5 +1,27 @@
+import { useRouter } from "next/router";
 import React from "react";
+import { getEventById } from "../../dummy-data";
+import NotFoundPage from "../404";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 export default function EventDetailPage() {
-  return <h1>EventDetailPage</h1>;
+  const router = useRouter();
+  
+  //this one can go ===> const eventId = router.query.eventid
+  const eventId = router.query["eventid"];
+
+  const event = getEventById(eventId);
+  if (!event) return <NotFoundPage />;
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics event={event} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
