59419c08db6858a295f45b05048676bd76ec85a2
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-12-05 08:26:47 +0100

    Add routes (pages)

---
+++ pages/events/[...slug].js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FilteredEventsPage() {
+  return <h1>FilteredEventsPage</h1>;
+}
---
+++ pages/events/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AllEventsPage() {
+  return <h1>AllEventsPage</h1>;
+}
