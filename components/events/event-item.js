5e44c7d26365dd24bc923a4a1674b4a6b4e71cfc
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-12-05 13:08:51 +0100

    added buttons and icons

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from "next/link";
+import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem({ event }) {
   const { title, image, date, location, id } = event;
@@ -12,20 +16,27 @@ export default function EventItem({ event }) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt="" />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href={exploreLink}>Explore event</Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,8 +1,9 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 export default function EventList({ events }) {
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map((event) => (
         <EventItem key={event.id} event={event} />
       ))}
