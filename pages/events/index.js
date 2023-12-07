f7e5412a36717289fa4140d0057510672f166a9d
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-12-07 09:14:10 +0100

    added all events to AllEvents page and featured events to home page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,20 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 
 export default function AllEventsPage() {
+  const router = useRouter();
   const allEvents = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={allEvents} />
     </>
   );
